a586581401bced9f0afdf8a1001016280e9e90dd
John McCants <johnmccants002@gmail.com>
2024-05-07 21:05:48 -0700

    Sign up and login screens created

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,9 +6,16 @@ type Props = {};
 const Layout = (props: Props) => {
   return (
     <Stack>
-      <Stack.Screen name="login" />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" />
+      <Stack.Screen
+        name="signup"
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerBackTitleVisible: false,
+        }}
+      />
     </Stack>
   );
 };
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,151 @@
 import React from "react";
+import {
+  Text,
+  View,
+  Image,
+  Pressable,
+  TextInput,
+  useWindowDimensions,
+  StyleSheet,
+} from "react-native";
+import { colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
 
 type Props = {};
 
 const Page = (props: Props) => {
-  return <></>;
+  const { height, width } = useWindowDimensions();
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/YumYap.png")}
+        style={[styles.logo, { width: width / 2 }]}
+      />
+      <Text style={styles.description}>
+        Simplifies cooking by magically transforming your cravings into detailed
+        recipes.
+      </Text>
+
+      <View
+        style={[
+          styles.card,
+          {
+            width: width,
+            justifyContent: "space-evenly",
+            paddingBottom: 80,
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            left: 0,
+            height: 500,
+          },
+        ]}
+      >
+        <View style={{ width: width, padding: 8, gap: 20 }}>
+          <View>
+            <Text style={styles.text}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter email..."
+              placeholderTextColor={colors.neutral["300"]}
+            />
+          </View>
+          <View>
+            <Text style={styles.text}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter password..."
+              placeholderTextColor={colors.neutral["300"]}
+            />
+          </View>
+        </View>
+        <View style={{ width: "100%", alignItems: "center" }}>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </Pressable>
+          <Pressable
+            style={styles.signUp}
+            onPress={() => router.push("/(auth)/signup")}
+          >
+            <Text style={styles.signUpText}>
+              Don't have an account?{" "}
+              <Text style={styles.signUpLink}>Sign Up</Text>
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary["100"],
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 80,
+  },
+  logo: {
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
+  description: {
+    fontSize: 16,
+    textAlign: "center",
+    marginVertical: 20,
+    color: colors.neutral["800"],
+    paddingHorizontal: 20,
+    fontFamily: "JakartaRegular",
+  },
+  text: {
+    fontSize: 16,
+    color: colors.neutral["800"],
+
+    fontFamily: "JakartaMedium",
+  },
+  card: {
+    backgroundColor: colors.neutral["50"],
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 20,
+  },
+  input: {
+    width: "90%",
+    height: 50,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: colors.neutral["300"],
+    borderRadius: 10,
+
+    backgroundColor: colors.neutral["200"],
+  },
+  button: {
+    width: "90%",
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: colors.primary["700"],
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: colors.whiteBlack["50"],
+    fontFamily: "InterSemibold",
+    fontSize: 16,
+  },
+  signUp: {
+    marginTop: 15,
+  },
+  signUpText: {
+    color: colors.neutral["600"],
+    fontFamily: "JakartaMedium",
+  },
+  signUpLink: {
+    color: colors.primary["700"],
+    fontWeight: "bold",
+  },
+});
 
 export default Page;
